856870e00dabd43b3426b0b591ad36dd76b5f944
Fernando Mauro <fermaurolf@gmail.com>
2023-08-30 21:17:07 -0600

    Clase 4 de react-router-dom terminado

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,3 +1,5 @@
+import { useRoutes,  BrowserRouter } from "react-router-dom"
+
 import { MyAccount } from "../MyAccount"
 import { Home } from '../Home'
 import { NotFound } from '../NotFound'
@@ -5,19 +7,24 @@ import { SignIn } from '../SignIn'
 import { MyOrder } from "../MyOrder"
 import { MyOrders } from "../MyOrders"
 
-function App() {
+const AppRoutes = () => {
 
-  return (
-    <>
-      <h1 className='bg-red-600'>Hola Mundo</h1>
-      <Home />
-      <MyAccount />
-      <MyOrder/>
-      <MyOrders/>
-      <NotFound />
-      <SignIn />
-    </>
-  )
+    const routes = useRoutes([
+        {path: "/", element: <Home />},
+        {path : "my-orders",element: <MyOrders/>},
+        {path : "my-account", element: <MyAccount/>},
+        {path : "sign-in", element: <SignIn/>},
+        {path : "my-order", element: <MyOrder/>},
+        {path : "*", element: <NotFound/>},
+    ]);
+
+    return routes;
 }
 
-export default App
+export const App = () => {
+    return (
+        <BrowserRouter>
+            <AppRoutes/>
+        </BrowserRouter>
+    )
+}
