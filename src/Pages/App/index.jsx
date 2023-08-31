0652689d013bf3f49dd7b393dd8595d6399cd10e
Fernando Mauro <fermaurolf@gmail.com>
2023-08-30 18:34:28 -0600

    Class 4 finished

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import { MyAccount } from "../MyAccount"
+import { Home } from '../Home'
+import { NotFound } from '../NotFound'
+import { SignIn } from '../SignIn'
+import { MyOrder } from "../MyOrder"
+import { MyOrders } from "../MyOrders"
 
 function App() {
 
   return (
     <>
       <h1 className='bg-red-600'>Hola Mundo</h1>
+      <Home />
+      <MyAccount />
+      <MyOrder/>
+      <MyOrders/>
+      <NotFound />
+      <SignIn />
     </>
   )
 }
